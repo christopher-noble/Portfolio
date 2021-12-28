3eb56554b4b2eb3edc11cc85fda396c0ca5a0c3c
christopher-noble <chris.noble@oceangold.ca>
2021-12-28 15:55:46 -0800

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  siteMetadata: {
+    siteUrl: "https://www.yourdomain.tld",
+    title: "portfolio",
+  },
+  plugins: [
+    {
+      resolve: "gatsby-plugin-google-analytics",
+      options: {
+        trackingId: "",
+      },
+    },
+  ],
+};
